8c9360c181c4b2e4b230ddca8c06290213ee0881
Robert Peteuil <robert@peteuil.com>
2019-07-02 07:42:25 -0700

    adjust formatting

--- product.py
+++ product.py
@@ -1,13 +1,15 @@
-from flask import Flask
-from flask import jsonify
-from pymongo import MongoClient
-import requests
-
 import copy
 import os
 import threading
 import time
 import uuid
+
+import consul
+from flask import Flask
+from flask import jsonify
+import requests
+from pymongo import MongoClient
+
 
 DB_ADDR = 'DB_ADDR'
 DB_PORT = 'DB_PORT'
@@ -19,6 +21,7 @@ COL_NAME = 'products'
 
 PRODUCT_PORT = 'PRODUCT_PORT'
 PRODUCT_ADDR = 'PRODUCT_ADDR'
+
 
 def connect_to_db():
     db_addr = os.environ.get(DB_ADDR)
@@ -34,7 +37,7 @@ def connect_to_db():
     return client
 
 
-## Consul Support
+# Consul Support
 class ProductConfig(object):
     def __init__(self):
         super().__init__()
@@ -53,7 +56,6 @@ class ProductConfig(object):
 
 
 def watch_config(config):
-    import consul
     index = None
     config.set(value=dict())
     c = None
@@ -65,11 +67,10 @@ def watch_config(config):
         except:
             time.sleep(1)
 
-
     while True:
         try:
-            index,data = c.kv.get("product/", index=index, recurse=True)
-            if data == None:
+            index, data = c.kv.get("product/", index=index, recurse=True)
+            if data is None:
                 config.set(dict())
             else:
                 new_data = dict()
@@ -79,11 +80,10 @@ def watch_config(config):
                     if value is not None:
                         value = value.decode('utf-8')
                         new_data[key] = value
-                new_data['datacenter']=c.agent.self().get('Config').get('Datacenter')
+                new_data['datacenter'] = c.agent.self().get('Config').get('Datacenter')
                 config.set(new_data)
         except (requests.exceptions.RequestException, consul.base.ConsulException):
             time.sleep(1)
-            pass
 
 
 db_client = connect_to_db()
@@ -93,16 +93,19 @@ thread.start()
 
 app = Flask(__name__)
 
+
 # these can be seeded into the DB for testing if necessary
-prods = [{ 'inv_id': 1, 'name':'jncos', 'cost':35.57, 'img':None},
-         { 'inv_id': 2, 'name':'denim vest', 'cost':22.50, 'img':None},
-         { 'inv_id': 3, 'name':'pooka shell necklace', 'cost':12.37, 'img':None},
-         { 'inv_id': 4, 'name':'shiny shirt', 'cost':17.95, 'img':None}]
+prods = [{'inv_id': 1, 'name': 'jncos', 'cost': 35.57, 'img': None},
+         {'inv_id': 2, 'name': 'denim vest', 'cost': 22.50, 'img': None},
+         {'inv_id': 3, 'name': 'pooka shell necklace', 'cost': 12.37, 'img': None},
+         {'inv_id': 4, 'name': 'shiny shirt', 'cost': 17.95, 'img': None}]
+
 
 @app.route("/product", methods=['GET'])
 def get_products():
     res = get_products_from_db()
     return jsonify({'res': res, 'instance_id': app.config['INSTANCE_ID'], 'conf': app.config['CONFIG'].get()})
+
 
 @app.route("/product/healthz", methods=['GET'])
 def get_health():
@@ -112,7 +115,7 @@ def get_health():
 
     if my_health is not None:
         return "UNHEALTHY", 503
-    elif global_health == None or global_health == 'true':
+    elif global_health is None or global_health == 'true':
         return "OK"
     else:
         return "UNHEALTHY", 503
@@ -121,11 +124,10 @@ def get_health():
 def get_products_from_db():
     return [rec for rec in db_client[DB_NAME][COL_NAME].find({}, {'_id': False})]
 
+
 if __name__ == '__main__':
     PORT = os.environ.get(PRODUCT_PORT)
     ADDR = os.environ.get(PRODUCT_ADDR)
-    INSTANCE_ID=str(uuid.uuid4())
-    app.config.update(
-            CONFIG=config,
-            INSTANCE_ID=INSTANCE_ID)
+    INSTANCE_ID = str(uuid.uuid4())
+    app.config.update(CONFIG=config, INSTANCE_ID=INSTANCE_ID)
     app.run(host=ADDR, port=PORT)
